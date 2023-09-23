016f4fc18ba7e755541289d841339cbe64bc6cc6
luizcesaralmeida <luizcesarmais44@hotmail.com>
2023-09-23 10:09:37 -0300

    primeiro commit

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,32 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : Controller
+{
+    private static List<FilmeDTO>  filmes = new List<FilmeDTO>();    
+
+    [HttpPost]
+    public IActionResult AdicionaFilme([FromBody] FilmeDTO filme)
+    {
+        filme.Id = filmes.Count + 1;
+        filmes.Add(filme);
+        return CreatedAtAction(nameof(ObterFilmePorId), new { id = filme.Id }, filme);
+    }
+    [HttpGet]
+    public IEnumerable<FilmeDTO> ObterFilmes([FromQuery] int skip = 0, [FromQuery] int take = 5)
+    {
+        return filmes.Skip(skip).Take(take);
+
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult ObterFilmePorId(int id)
+    {        
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        return filme is null ? NotFound() : Ok(filme);
+    }
+}
---
+++ Models/FilmeDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace FilmesAPI.Models;
+
+public class FilmeDTO
+{
+    private const int MAX_LENGTH  = 700;
+    private const int MIN_LENGTH  = 70;
+
+    [JsonIgnore]
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Título é obrigatório")]
+    [StringLength(100, ErrorMessage = "Tamanho não pode ser maior que 100 caracteres")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "Genêro é obrigatório")]
+    public string Genero { get; set; }
+
+    [Required(ErrorMessage = "Duração é obrigatório")]
+    [Range(MIN_LENGTH, MAX_LENGTH, ErrorMessage = "Tamanho maior que o intervalo permitido entre 70 e 700")]
+    public int? Duracao{ get; set; }
+}
